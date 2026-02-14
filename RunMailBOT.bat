a7a140a7a8f95e46e536c9b1fa3e94fd53ea0ff2
saharatchaimetta <156056224+saharatchaimetta@users.noreply.github.com>
2026-02-14 16:15:48 +0700

    Add files via upload

---
+++ RunMailBOT.bat
@@ -0,0 +1,13 @@
+@echo off
+title RunMailBOT
+color 2
+echo ============================================
+echo           BOT CREDIT INFORMATION
+echo ============================================
+echo.
+echo    This bot was created by: Saharat Jaimetta
+echo.
+echo ============================================
+cd c:\users\user\desktop\python projects\detect_button
+echo.
+py main.py
