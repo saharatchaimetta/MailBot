6062707a30d5b2bcfc446a4a64f0eff78849ded4
MRPROPZ <78914920+MRPROPZ@users.noreply.github.com>
2026-02-14 16:39:01 +0700

    Update RunMailBOT.bat

--- RunMailBOT.bat
+++ RunMailBOT.bat
@@ -6,8 +6,10 @@ echo           BOT CREDIT INFORMATION
 echo ============================================
 echo.
 echo    This bot was created by: Saharat Jaimetta
+echo.                            & MRPROPZ
 echo.
 echo ============================================
 cd c:\users\user\desktop\python projects\detect_button
 echo.
 py main.py
+
